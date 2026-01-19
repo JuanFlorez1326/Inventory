f9725e81f9f4679545079df142862d37fb8665fb
Juan Florez <juanks339@gmail.com>
2026-01-19 16:57:17 -0500

    add: Base folder structure

---
+++ frontend/src/app/features/products/services/products.service.ts
@@ -0,0 +1,30 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
+import { environment } from '../../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Products {
+
+  private readonly apiUrl = environment.apiUrl;
+  private readonly http = inject(HttpClient);
+
+  public getProducts(): Observable<Product[]> {
+    const url = `${this.apiUrl}/products`;
+    return this.http.get<Product[]>(url);
+  }
+
+  public createProduct(product: Product): Observable<Product> {
+    const url = `${this.apiUrl}/products`;
+    return this.http.post<Product>(url, product);
+  }
+
+  public deleteProduct(productId: string): Observable<void> {
+    const url = `${this.apiUrl}/products/${productId}`;
+    return this.http.delete<void>(url);
+  }
+  
+}
